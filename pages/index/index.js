a39a46fdae8a4999519de42431c601cd41c46651
westlin <932420983@qq.com>
2019-09-24 19:08:41 +0800

    首页楼层

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,19 +2,47 @@ Page({
 
   data: {
     swiperList: [],
+    catitems:[],
+    Floordata:[]
   },
 
   onLoad(){
-    this.getswiperList()
+    this.getSwiperList();
+    this.getCatitems();
+    this.getFloordata();
   },
-
-  getswiperList() {
+  // 轮播图
+  getSwiperList() {
     wx.request({
       url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
       success: (res) => {
-        console.log(res);
+        // console.log(res);
         this.setData({
           swiperList: res.data.message
+        })
+      }
+    })
+  },
+  // 导航栏
+  getCatitems(){
+    wx.request({
+      url:"https://api.zbztb.cn/api/public/v1/home/catitems",
+      success:(res) => {
+        // console.log(res);
+        this.setData({
+          catitems:res.data.message
+        })
+      }
+    })
+  },
+  // 楼层
+  getFloordata (){
+    wx.request({
+      url:"https://api.zbztb.cn/api/public/v1/home/floordata",
+      success: (res) => {
+        // console.log(res);
+        this.setData({
+          Floordata:res.data.message
         })
       }
     })
